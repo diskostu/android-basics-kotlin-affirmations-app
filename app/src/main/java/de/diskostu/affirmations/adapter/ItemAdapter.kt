eb03d0f635be370c6603089ef16738fad7d81a18
diskostu <denny.kluge@gmail.com>
2022-01-11 08:53:46 +0100

    added images

--- app/src/main/java/de/diskostu/affirmations/adapter/ItemAdapter.kt
+++ app/src/main/java/de/diskostu/affirmations/adapter/ItemAdapter.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import de.diskostu.affirmations.R
@@ -20,6 +21,7 @@ class ItemAdapter(
      */
     class ItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val textView: TextView = view.findViewById(R.id.item_title)
+        val imageView: ImageView = view.findViewById(R.id.item_image)
     }
 
     /**
@@ -37,6 +39,7 @@ class ItemAdapter(
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         val item = dataset[position]
         holder.textView.text = context.resources.getString(item.stringResourceId)
+        holder.imageView.setImageResource(item.imageResourceId)
     }
 
     /**
